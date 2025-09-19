b5aa04ffd0fb045b015601527be3800a8c30bd01
Thalles Campagnani <thallescampagnani@gmail.com>
2025-09-19 22:42:20 +0000

    Convertendo script para distribuição Debian

--- 2-installGNU.sh
+++ 2-installGNU.sh
@@ -5,9 +5,9 @@ echo """
 ##############################################################
 ######                                                  ######
 ######  Instalação do Sistema Operacional GNU para URT  ######
-######               Unidade do Reator Triga            ######
+######             Unidade do Reator Triga              ######
 ######                                                  ######
-######          Distribuição: ArchLinux Rolling         ######
+######          Distribuição: Debian 13 (Trixie)        ######
 ######                                                  ######
 ######         Autor: Thalles Oliveira Campagnani       ######
 ######                                                  ######
@@ -24,68 +24,89 @@ echo """
 """
 read -p "Pressione enter para iniciar... "
 
+# Garante que o script pare se algum comando falhar
 set -e
+
+# Verifica se debootstrap está instalado
+if ! command -v debootstrap &> /dev/null; then
+    echo "Erro: O comando 'debootstrap' não foi encontrado."
+    echo "Por favor, instale o pacote 'debootstrap' no seu sistema atual e tente novamente."
+    exit 1
+fi
+
+# Path das partições
+EFI_PART="/dev/nvme0n1p1"
+DEBIAN_PART="/dev/nvme0n1p5"
+HOME_PART="/dev/nvme0n1p6"
 
 # Atualizar relógio
 echo "{[( Atualizando relógio )]}"
 timedatectl
 
 # Configuração das partições
-echo "{[( Configuração das partições )]}"
+echo "{[( Configuração atual das partições )]}"
 fdisk -l
 
 echo """
-# Configuração das partições esperada pelo instalador:
-    #/dev/nvme0n1p1 - EFI
-    #/dev/nvme0n1p2 - MS Reserved
-    #/dev/nvme0n1p3 - Windows CDTN
-    #/dev/nvme0n1p4 - Arch
-    #/dev/nvme0n1p5 - Debian
-    #/dev/nvme0n1p6 - Home
+# Configuração esperada das partições (por este instalador):
+    # /dev/nvme0n1p1 - EFI
+    # /dev/nvme0n1p2 - MS Reserved
+    # /dev/nvme0n1p3 - Windows CDTN
+    # /dev/nvme0n1p4 - Arch
+    # /dev/nvme0n1p5 - Debian (Será formatada e usada)
+    # /dev/nvme0n1p6 - Home (Será formatada e usada)
 """
-# Formatar a partição raiz
-read -p "Deseja formatar a partição raiz (/dev/nvme0n1p4)? [y/N]: " formatar_raiz
+# Formatar a partição raiz do Debian
+read -p "Deseja formatar a partição raiz (${DEBIAN_PART})? [y/N]: " formatar_raiz
 if [[ $formatar_raiz == "y" || $formatar_raiz == "Y" ]]; then
-    echo "Formatando partição raiz..."
-    mkfs.ext4 /dev/nvme0n1p4
+    echo "Formatando partição raiz do Debian..."
+    mkfs.ext4 ${DEBIAN_PART}
 fi
 
 # Formatar a partição home
-read -p "Deseja formatar a partição home (/dev/nvme0n1p6)? [y/N]: " formatar_home
+read -p "Deseja formatar a partição home (${HOME_PART})? [y/N]: " formatar_home
 if [[ $formatar_home == "y" || $formatar_home == "Y" ]]; then
     echo "Formatando partição home..."
-    mkfs.ext4 /dev/nvme0n1p6
+    mkfs.ext4 ${HOME_PART}
 fi
 
 # Montar partições
 echo "{[( Montando partições )]}"
-mount /dev/nvme0n1p4 /mnt
-mount --mkdir /dev/nvme0n1p6 /mnt/home
-mount --mkdir /dev/nvme0n1p1 /mnt/boot/efi
+mount ${DEBIAN_PART} /mnt
+mount --mkdir ${HOME_PART} /mnt/home
+mount --mkdir ${EFI_PART} /mnt/boot/efi
 
-# Configuração dos repositórios
-echo "{[( Configurando repositórios )]}"
-systemctl stop reflector.service
-cp -f mirrorlist /etc/pacman.d/mirrorlist
-cp -f pacman.conf /etc/pacman.conf
-#pacman -Sy archlinux-keyring
-#pacman -Sy
-
-# Instalação do sistema base
-echo "{[( Instalando sistema base )]}"
-pacstrap -K /mnt base linux linux-firmware intel-ucode grub efibootmgr nano vim e2fsprogs ntfs-3g networkmanager sudo openssh python dbus dbus-python
+# Instalação do sistema base com debootstrap
+echo "{[( Instalando sistema base do Debian 13 (Trixie) )]}"
+debootstrap --arch=amd64 --components=main,contrib,non-free,non-free-firmware --include=firmware-linux,intel-microcode trixie /mnt http://debian.c3sl.ufpr.br/debian/ #http://deb.debian.org/debian/
 
 # Gerar fstab
 echo "{[( Gerando fstab )]}"
-genfstab -U /mnt >> /mnt/etc/fstab
+# O Debian não tem genfstab, então geramos manualmente com UUIDs para maior robustez
+{
+    echo "# <file system> <mount point>   <type>  <options>       <dump>  <pass>"
+    echo "UUID=$(blkid -s UUID -o value ${DEBIAN_PART}) /               ext4    errors=remount-ro 0       1"
+    echo "UUID=$(blkid -s UUID -o value ${HOME_PART})   /home           ext4    defaults        0       2"
+    echo "UUID=$(blkid -s UUID -o value ${EFI_PART})    /boot/efi       vfat    umask=0077      0       1"
+} > /mnt/etc/fstab
+
+cat /mnt/etc/fstab
 
 # Copiar script para pasta root
 echo "{[( Copiando script de configuração para pasta root )]}"
-cp 3-configGNU.sh /mnt/root
-cp eduroam-linux-CDTN.py /mnt/root
-cp mirrorlist /mnt/root
-cp pacman.conf /mnt/root
+cp 3-configGNU.sh /mnt/root/
+
+# Montar sistemas de arquivos virtuais para o chroot
+echo "{[( Preparando o ambiente para chroot )]}"
+sudo mount --bind /dev /mnt/dev
+sudo mount --bind /dev/pts /mnt/dev/pts
+sudo mount -t proc proc /mnt/proc
+sudo mount -t sysfs sysfs /mnt/sys
 
 # Fazer chroot e executar script de configuração
 echo "{[( Fazendo chroot e executando script de configuração )]}"
-arch-chroot /mnt /root/3-configGNU.sh
+chroot /mnt /bin/bash --login -c "/root/3-configDebian.sh"
+
+# Desmontar tudo após a finalização
+echo "{[( Desmontando partições )]}"
+umount -R /mnt
